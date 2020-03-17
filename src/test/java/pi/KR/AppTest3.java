f77e3b752b6a1fdaeb82da6249561c0ec54a508e
Max <kitakk17@gmail.com>
2020-03-17 17:31:12 +0500

    first

---
+++ src/test/java/pi/KR/AppTest3.java
@@ -0,0 +1,16 @@
+package pi.KR;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/* Метод, тестирующий вычисления ОК прямоугольного типа, используя метод assertEquals(),
+ * принимающий в качестве параметров ответ (число) и число, возвращенное методом calculation(). */
+public class AppTest3 {
+	
+	CalcMain api = new CalcMain();
+	@Test
+   	
+	public void testApp() {
+		assertEquals(256, api.calculation(15, 15));
+	}
+	
+}
